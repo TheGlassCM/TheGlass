fed8e93c5e2afe51bbec4183cb6a7c82b490fef8
AlejandroMolinosEligio <alejandromoeli@gmail.com>
2023-03-06 11:26:15 +0100

    Cambios de username y photo del usuario; Login de bares

---
+++ app/src/main/java/com/example/loginregister/loginBarActivity.java
@@ -0,0 +1,14 @@
+package com.example.loginregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_bar);
+    }
+}
