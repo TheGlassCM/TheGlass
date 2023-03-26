f23886e4e6e0b5af950ab57550e2501b0fbaa8ac
pabgalace <pablogaac@gmail.com>
2023-03-26 22:57:30 +0200

    Mapa fragmentMap  - Mapa funcional, aún en desarrollo.

--- app/src/main/java/com/example/loginregister/fragmentMap.java
+++ app/src/main/java/com/example/loginregister/fragmentMap.java
@@ -1,19 +1,214 @@
 package com.example.loginregister;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.example.loginregister.utils.JsonParser;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 public class fragmentMap extends Fragment {
+
+    private Spinner spType;
+    private Button btFind;
+    private SupportMapFragment supportMapFragment;
+    private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private double currentLat = 0, currentLong = 0;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        spType = view.findViewById(R.id.sp_type);
+        btFind = view.findViewById(R.id.bt_find);
+        supportMapFragment = (SupportMapFragment) getParentFragmentManager().findFragmentById(R.id.google_map);
+
+        // Bar types
+        String[] placeTypeArray = {"glass_pub", "beer_pub"};
+        // Bar names
+        String[] placeNameArray = {"Bar de copas", "Cerveceria"};
+
+        // Setting the names that will appear in the adapter
+        spType.setAdapter(new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, placeNameArray));
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        btFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = spType.getSelectedItemPosition();
+                String url =  "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                        "?location=" + currentLat + "," + currentLong
+                        + "&radius=5000" // Nearby radius
+                        + "&type=" + placeTypeArray[i] + "&sensor=true"
+                        + "&key=" + getResources().getString(R.string.google_map_key);
+                new PlaceTask().execute(url);
+            }
+        });
+
+    }
+
+    private void getCurrentLocation() {
+
+        if (ActivityCompat.checkSelfPermission(requireView().getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if(location != null){
+                        // Once we get the location
+                        // Get current latitude
+                        currentLat = location.getLatitude();
+                        // Get current longitude
+                        currentLong = location.getLongitude();
+                        // Sync map
+                        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(GoogleMap googleMap) {
+                                map = googleMap;
+                                map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLat,currentLong), 10));
+                            }
+                        });
+                    }
+                }
+            });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            new ParserTask().execute(s);
+        }
+    }
+
+    private String downloadUrl(String string) throws IOException {
+        URL url = new URL(string);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+        InputStream stream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+        while ((line = reader.readLine()) != null){
+            builder.append(line);
+        }
+        String data = builder.toString();
+        reader.close();
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String,Integer, List<HashMap<String,String>>>{
+
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            JsonParser jsonParser = new JsonParser();
+
+            List<HashMap<String,String>> mapList = null;
+            JSONObject object = null;
+            try {
+                object = new JSONObject(strings[0]);
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            map.clear();
+            for(HashMap<String,String> hashMap : hashMaps){
+                // Getting latitude
+                double lat = Double.parseDouble(hashMap.get("lat"));
+                // Getting longitude
+                double lng = Double.parseDouble(hashMap.get("lng"));
+                // Getting name
+                String name = hashMap.get("name");
+                // Concat latitude and longitude
+                LatLng latLng = new LatLng(lat,lng);
+                // Initialize marker options
+                MarkerOptions options = new MarkerOptions();
+                // Set position
+                options.position(latLng);
+                // Set title
+                options.title(name);
+                // Add marker on map
+                map.addMarker(options);
+            }
+        }
     }
 }
